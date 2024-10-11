501741e5dc29aa52746aa1c200927e0b6a972861
BergerAPI <bergerbtw@gmail.com>
2024-10-11 23:45:34 +0200

    feat: advanced tracing of requests

--- worker/cmd/worker.go
+++ worker/cmd/worker.go
@@ -1,30 +1,80 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
+	"fmt"
 	"github.com/nats-io/nats.go"
-	"io"
 	"log"
 	"net/http"
+	"net/http/httptrace"
 	"sync"
+	"time"
 )
 
-func sendRequest(requestURL string) (string, error) {
-	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+// TimingData holds the duration of various phases of the request lifecycle
+type TimingData struct {
+	DNSDuration     time.Duration
+	ConnectDuration time.Duration
+	TLSDuration     time.Duration
+	TimeToFirstByte time.Duration
+	TotalDuration   time.Duration
+}
+
+// newClientTrace creates a new httptrace.ClientTrace to track request timings
+func newClientTrace(timings *TimingData, start time.Time) *httptrace.ClientTrace {
+	var dnsStart, connectStart, tlsStart time.Time
+
+	return &httptrace.ClientTrace{
+		DNSStart: func(_ httptrace.DNSStartInfo) {
+			dnsStart = time.Now()
+		},
+		DNSDone: func(_ httptrace.DNSDoneInfo) {
+			timings.DNSDuration = time.Since(dnsStart)
+		},
+		ConnectStart: func(_, _ string) {
+			connectStart = time.Now()
+		},
+		ConnectDone: func(_, _ string, _ error) {
+			timings.ConnectDuration = time.Since(connectStart)
+		},
+		TLSHandshakeStart: func() {
+			tlsStart = time.Now()
+		},
+		TLSHandshakeDone: func(_ tls.ConnectionState, _ error) {
+			timings.TLSDuration = time.Since(tlsStart)
+		},
+		GotFirstResponseByte: func() {
+			timings.TimeToFirstByte = time.Since(start)
+		},
+	}
+}
+
+// TraceRequest sends an HTTP GET request and traces the request lifecycle
+func TraceRequest(targetURL string) (*TimingData, error) {
+	req, err := http.NewRequest("GET", targetURL, nil)
 	if err != nil {
-		return "", err
+		return nil, fmt.Errorf("failed to create request: %w", err)
 	}
 
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", err
-	}
+	start := time.Now()
+	timings := &TimingData{}
 
-	resBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		return "", err
-	}
+	// Attach the trace to the request's context
+	req = req.WithContext(httptrace.WithClientTrace(context.Background(), newClientTrace(timings, start)))
 
-	return string(resBody), nil
+	// Perform the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Calculate total duration after receiving the response
+	timings.TotalDuration = time.Since(start)
+
+	return timings, nil
 }
 
 func main() {
@@ -36,8 +86,8 @@ func main() {
 
 	// Subscribe
 	if _, err := nc.Subscribe("updates", func(m *nats.Msg) {
-		res, _ := sendRequest(string(m.Data))
-		println(res)
+		res, _ := TraceRequest(string(m.Data))
+		println(res.DNSDuration.String())
 	}); err != nil {
 		log.Fatal(err)
 	}
