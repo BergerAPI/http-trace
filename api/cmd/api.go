80becd01d52b02ad7f963a5580d963e8841924e9
BergerAPI <bergerbtw@gmail.com>
2024-10-13 13:24:22 +0200

    feat: basic api pattern to trace an url

--- api/cmd/api.go
+++ api/cmd/api.go
@@ -1,16 +1,48 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/nats-io/nats.go"
+	"log"
+	"net/url"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/health", func(c *fiber.Ctx) error {
-		return c.SendString("OK")
+	// Connecting to the NATS-Server
+	nc, err := nats.Connect(nats.DefaultURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	app.Get("/health", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("OK")
+	})
+
+	app.Post("/v1/probe", func(ctx *fiber.Ctx) error {
+		payload := struct {
+			Region string `json:"region"`
+			Target string `json:"target"`
+		}{}
+
+		if err := ctx.BodyParser(&payload); err != nil {
+			return err
+		}
+
+		// Checking whether the target is a valid url
+		targetUrl, err := url.ParseRequestURI(payload.Target)
+		if err != nil {
+			return ctx.Status(401).SendString("Target is missing or invalid. Refer to the official documentation.")
+		}
+
+		if err := nc.Publish("jobs.eu.trace", []byte(targetUrl.String())); err != nil {
+			return err
+		}
+
+		return ctx.SendString("OK")
 	})
 
 	log.Fatal(app.Listen(":3000"))
